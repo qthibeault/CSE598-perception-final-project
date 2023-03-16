3853effda5f26313fd82960a4d694527083b1724
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-03-15 18:16:51 -0700

    Implement initial sub-pub detector node

--- src/detector/detector/detector.py
+++ src/detector/detector/detector.py
@@ -1,2 +1,26 @@
-def main():
-    pass
+from __future__ import annotations
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class Detector(Node):
+    def __init__(self) -> None:
+        super().__init__("detector")
+        self._publisher = self.create_publisher(String, "topic", 10)
+        self._subscription = self.create_subscription(String, "topic", self.on_message, 10)
+        self.get_logger().info("--- [ Detector node online ]")
+
+    def on_message(self, msg: String) -> None:
+        pass
+
+
+def main(args: list[str] | None = None) -> None:
+    rclpy.init(args=args)
+    rclpy.spin(Detector())
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
