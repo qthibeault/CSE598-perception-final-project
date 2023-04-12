a35ccbd9abb221f7e489e765e868bc150f3a3e79
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-04-12 00:27:48 -0700

    Complete converter package convert module
    Fix parameter definition to include default value. Include a check to
    ensure that the video file name is not empty. Define a custom exception
    type that represents when no more video frames are available. Handle
    exception for graceful node shutdown when video has been transmitted
    completely.

--- src/converter/converter/convert.py
+++ src/converter/converter/convert.py
@@ -5,12 +5,20 @@ import rclpy.node as node
 import sensor_msgs.msg as msg
 
 
+class FramesExhausted(Exception):
+    pass
+
+
 class Converter(node.Node):
     def __init__(self):
         super().__init__("converter", parameter_overrides=[])
-        self.declare_parameter("video")
 
-        self._video_name = self.get_parameter("video").get_parameter_value().string_value
+        self.declare_parameter("video", "")
+        self._video_name: str = self.get_parameter("video").get_parameter_value().string_value
+
+        if len(self._video_name) == 0:
+            raise ValueError("Video name cannot be empty")
+
         self._video = cv2.VideoCapture(self._video_name)
 
         if not self._video.isOpened():
@@ -19,6 +27,7 @@ class Converter(node.Node):
         self._bridge = cv_bridge.CvBridge()
         self._image_topic = self.create_publisher(msg.Image, "images", 10)
         self._publish_timer = self.create_timer(0.1, self._send_image)
+        self._frame = 1
 
     def _send_image(self):
         # Retrieve the next frame from the video
@@ -28,18 +37,27 @@ class Converter(node.Node):
         if not read_success:
             # Close video file and stop publish timer
             self._video.release()
-            self._publish_timer.cancel()
+            self.get_logger().debug(f"Finished with {self._frame} frames")
 
-        # Convert the OpenCV Mat to a ROS2 Image
-        msg = self._bridge.cv2_to_imgmsg(frame)
+            raise FramesExhausted()
+        else:
+            # Convert the OpenCV Mat to a ROS2 Image
+            msg = self._bridge.cv2_to_imgmsg(frame)
 
-        # Publish frame to topic
-        self._image_topic.publish(msg)
+            # Publish frame to topic
+            self._image_topic.publish(msg)
+            self.get_logger().debug(f"Sent frame {self._frame}")
+            self._frame += 1
 
 
 def main(args: list[str] | None = None):
     rclpy.init(args=args)
-    rclpy.spin(Converter())
+
+    try:
+        rclpy.spin(Converter())
+    except FramesExhausted:
+        pass
+
     rclpy.shutdown()
 
 
