381e7d4d935ea0a12c1f749a11e19c68db9fe5e2
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-04-19 11:25:24 -0700

    Baseline implementation

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+import logging
+import math
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import TYPE_CHECKING, Iterable, Iterator, Optional, Protocol, Type
-from uuid import UUID, uuid4
+from random import randint
+from signal import SIGINT, SIGTERM, signal
+from typing import TYPE_CHECKING, Any, Iterable, Iterator, Optional, Protocol, Type
 
 import click
 import cv2
@@ -21,31 +24,34 @@ class Point:
     x: float
     y: float
 
-    def as_tuple(self) -> tuple[float, float]:
-        return (self.x, self.y)
+    def distance(self, other: Point) -> float:
+        return math.sqrt(math.pow(self.x - other.x, 2) + math.pow(self.y - other.y, 2))
+
+    def as_tuple(self, *, dtype: Type[float] | Type[int] = float) -> tuple[float, float]:
+        return (dtype(self.x), dtype(self.y))
 
 
 @dataclass(frozen=True)
 class BBox:
     """The bounding box of an object."""
 
-    x: tuple[float, float] = field()
-    y: tuple[float, float] = field()
+    p1: Point = field()
+    p2: Point = field()
 
     def __post_init__(self):
-        if self.x[0] >= self.x[1]:
+        if self.p1.x >= self.p2.x:
             raise ValueError("x0 must be strictly less than x1")
 
-        if self.y[0] >= self.y[1]:
+        if self.p1.y >= self.p2.y:
             raise ValueError("y0 must be strictly less than y1")
 
     @property
     def width(self) -> float:
-        return self.x[1] - self.x[0]
+        return self.p2.x - self.p1.x
 
     @property
     def height(self) -> float:
-        return self.y[1] - self.y[0]
+        return self.p2.y - self.p1.y
 
     @property
     def area(self) -> float:
@@ -53,19 +59,16 @@ class BBox:
 
     @property
     def center(self) -> Point:
-        x_center = (self.x[1] - self.x[0]) / 2
-        y_center = (self.y[1] - self.y[0]) / 2
-
-        return Point(x_center, y_center)
+        return Point(self.p1.x + self.width / 2, self.p1.y + self.height / 2)
 
     def iou(self, other: BBox) -> float:
         """Compute the Intersection-over-Union (IOU) score between two bounding boxes."""
 
-        x1 = max(self.x[0], other.x[0])
-        x2 = min(self.x[1], other.x[1])
+        x1 = max(self.p1.x, other.p1.x)
+        x2 = min(self.p2.x, other.p2.x)
 
-        y1 = max(self.y[0], other.y[0])
-        y2 = min(self.y[1], other.y[1])
+        y1 = max(self.p1.y, other.p1.y)
+        y2 = min(self.p2.y, other.p2.y)
 
         # Handle the case where the two bounding boxes do not intersect
         if x2 < x1 or y2 < y1:
@@ -76,10 +79,9 @@ class BBox:
 
         return inter_area / union_area
 
-    def draw(self, img: cv2.Mat):
-        start = (self.x[0], self.y[0])
-        end = (self.x[1], self.y[1])
-        color = (255, 0, 0)
+    def draw(self, img: cv2.Mat, color: tuple[int, int, int]):
+        start = self.p1.as_tuple(dtype=int)
+        end = self.p2.as_tuple(dtype=int)
         thickness = 2
 
         cv2.rectangle(img, start, end, color, thickness)
@@ -89,29 +91,52 @@ class BBox:
         """Create a bounding box from a center-point."""
 
         x_dist = width / 2
-        x = (pt.x - x_dist, pt.x + x_dist)
-
         y_dist = height / 2
-        y = (pt.y - y_dist, pt.y + y_dist)
 
-        return BBox(x, y)
+        p1 = Point(pt.x - x_dist, pt.y - y_dist)
+        p2 = Point(pt.x + x_dist, pt.y + y_dist)
+
+        return BBox(p1, p2)
+
+
+def _random_color() -> tuple[int, int, int]:
+    return (randint(0, 255), randint(0, 255), randint(0, 255))
 
 
 @dataclass(frozen=True)
 class Object:
-    bbox: BBox = field()
-    history: list[BBox] = field(default_factory=list)
-    ident: UUID = field(init=False, default_factory=uuid4)
+    bbox: BBox = field(hash=False)
+    history: list[BBox] = field(default_factory=list, hash=False)
+    color: tuple[int, int, int] = field(default_factory=_random_color)
+
+    @property
+    def id(self) -> int:
+        return hash(self.color)
+
+    @property
+    def largest_bbox(self) -> BBox:
+        if len(self.history) == 0:
+            return self.bbox
+
+        prev_best = max(self.history, key=lambda b: b.area)
+        return max(self.bbox, prev_best, key=lambda b: b.area)
 
     def best_match(self, bboxes: Iterable[BBox]) -> tuple[BBox, float]:
         scores = ((bbox, self.bbox.iou(bbox)) for bbox in bboxes)
         return max(scores, key=lambda s: s[1])
 
     def advance(self, bbox: BBox) -> Object:
-        return Object(bbox, [self.bbox] + self.history)
+        return Object(bbox, [self.bbox] + self.history, self.color)
 
     def draw(self, img: cv2.Mat):
-        self.bbox.draw(img)
+        self.bbox.draw(img, self.color)
+        cv2.circle(
+            img,
+            center=self.bbox.center.as_tuple(dtype=int),
+            radius=5,
+            color=self.color,
+            thickness=-1,
+        )
 
 
 def load_frames(capture: cv2.VideoCapture) -> Iterator[cv2.Mat]:
@@ -136,27 +161,50 @@ def _bbox_from_tensor(tensor: torch.Tensor) -> BBox:
     if tensor.size(0) != 4:
         raise ValueError("Bounding box tensor must contain exactly 4 elements")
 
-    x_interval = (tensor[0].item(), tensor[2].item())
-    y_interval = (tensor[1].item(), tensor[3].item())
+    p1 = Point(tensor[0].item(), tensor[1].item())
+    p2 = Point(tensor[2].item(), tensor[3].item())
 
-    return BBox(x_interval, y_interval)
+    return BBox(p1, p2)
 
 
-def detect(model: models.MaskRCNN, image: torch.Tensor) -> set[BBox]:
+def detect(
+    model: models.MaskRCNN,
+    image: torch.Tensor,
+    *,
+    labels: list[str],
+    allowed: list[str],
+) -> set[BBox]:
     """Create a set of bounding boxes from objects in an image."""
+    logger = logging.getLogger("detection")
+    logger.debug("Beginning detection")
 
     batch = image.unsqueeze(0)
     results = model(batch)
     result = results[0]
+    bbox_labels = [labels[int(label_idx.item())] for label_idx in result["labels"]]
 
-    return {_bbox_from_tensor(box) for box in result["boxes"]}
+    if len(allowed) == 0:
+        detections = {_bbox_from_tensor(box) for box in result["boxes"]}
+        logger.debug(f"Labels in frame: {bbox_labels}")
+    else:
+        detections = {
+            _bbox_from_tensor(box)
+            for box, label in zip(result["boxes"], bbox_labels)
+            if label in allowed
+        }
+
+    logger.debug(f"Found {len(detections)} objects in frame")
+    return detections
 
 
 class Predictor(Protocol):
     def __call__(self, target: BBox) -> BBox:
         ...
 
-    def draw(self, img: cv2.Mat) -> None:
+    def best_match(self, bboxes: Iterable[BBox]) -> tuple[BBox, float]:
+        ...
+
+    def draw(self, img: cv2.Mat, color: tuple[int, int, int]) -> None:
         ...
 
 
@@ -191,9 +239,10 @@ class PLine:
         y_perp_eq = y_eq - point.y
         dot_prod = x_perp_eq * self.delta_x + y_perp_eq * self.delta_y
 
-        sol = solve(dot_prod, dict=True)
+        solutions: list[dict[str, float]] = solve(dot_prod, dict=True)
+        assert len(solutions) == 1
 
-        return self(sol["t"])
+        return self(solutions[0][t])
 
     @classmethod
     def from_points(cls: Type[Self], p1: Point, p2: Point) -> Self:
@@ -203,26 +252,33 @@ class PLine:
 @dataclass(frozen=True)
 class LinearPredictor(Predictor):
     line: PLine
-    width: float
-    height: float
+    bbox: BBox
 
     def __call__(self, target: BBox) -> BBox:
         pt = self.line.closest_point(target.center)
-        return BBox.from_center(pt, self.width, self.height)
+        return BBox.from_center(pt, self.bbox.width, self.bbox.height)
 
-    def draw(self, img: cv2.Mat):
-        p1 = self.line(0).as_tuple()
-        p2 = self.line(1).as_tuple()
-        p3 = self.line(100).as_tuple()
+    def best_match(self, bboxes: Iterable[BBox]) -> tuple[BBox, float]:
+        def _future_iou(bbox: BBox) -> float:
+            future_bbox = self(bbox)
+            return self.bbox.iou(future_bbox)
+
+        scores = ((bbox, _future_iou(bbox)) for bbox in bboxes)
+        return max(scores, key=lambda s: s[1])
+
+    def draw(self, img: cv2.Mat, color: tuple[int, int, int]):
+        p1 = self.line(0).as_tuple(dtype=int)
+        p2 = self.line(1).as_tuple(dtype=int)
+        p3 = self.line(100).as_tuple(dtype=int)
 
         cv2.circle(img, p1, radius=0, color=(0, 0, 255), thickness=-1)
         cv2.circle(img, p2, radius=0, color=(0, 0, 255), thickness=-1)
-        cv2.line(img, p1, p3, color=(0, 255, 0), thickness=2)
+        cv2.line(img, p1, p3, color=color, thickness=2)
 
 
-def _predict_linear(curr_bbox: BBox, prev_centers: list[Point]) -> Predictor:
-    line = PLine.from_points(prev_centers[0], curr_bbox.center)
-    return LinearPredictor(line, curr_bbox.width, curr_bbox.height)
+def _predict_linear(obj: Object) -> Predictor:
+    line = PLine.from_points(obj.history[0].center, obj.bbox.center)
+    return LinearPredictor(line, obj.largest_bbox)
 
 
 def predict(obj: Object, method: str) -> Predictor:
@@ -230,82 +286,131 @@ def predict(obj: Object, method: str) -> Predictor:
         raise ValueError("Cannot predict the trajectory of an object with no history")
 
     if method == "linear":
-        return _predict_linear(obj.bbox, [bbox.center for bbox in obj.history])
+        return _predict_linear(obj)
 
     raise ValueError(f"Unknown interpolation method {method}")
 
 
+@dataclass(frozen=True)
+class TrackingResult:
+    obj: Object = field()
+
+
+@dataclass(frozen=True)
+class ProxUpdate(TrackingResult):
+    pass
+
+
+@dataclass(frozen=True)
+class PredUpdate(TrackingResult):
+    predictor: Predictor = field(hash=False)
+
+
+@dataclass(init=False, frozen=True)
+class NewObject(TrackingResult):
+    def __init__(self, bbox: BBox):
+        super().__init__(Object(bbox))
+
+
+@dataclass(frozen=True)
+class Unchanged(TrackingResult):
+    pass
+
+
 def track(
-    objs: Iterable[Object],
+    objs: set[Object],
     bboxes: set[BBox],
     *,
     method: str,
-    tol: float = 0.9,
-) -> tuple[list[Object], list[Predictor]]:
-    tracked = []
-    predictors = []
+    tol: float = 0.8,
+) -> list[TrackingResult]:
+    logger = logging.getLogger("tracking")
+    logger.debug(f"Beginning tracking with {len(objs)} objects and {len(bboxes)} detections")
 
-    # For every object assign the best detection match as its new current position
+    results: list[TrackingResult] = []
+    unassigned_objs = set()
+
     for obj in objs:
-        bbox, iou = obj.best_match(bboxes)
+        if len(bboxes) == 0:
+            unassigned_objs.add(obj)
+        else:
+            bbox, iou = obj.best_match(bboxes)
+            if iou > tol:
+                logger.debug(f"Object {obj.id} assigned detection from IoU score")
+                bboxes.remove(bbox)
+                results.append(ProxUpdate(obj.advance(bbox)))
+            else:
+                unassigned_objs.add(obj)
 
-        if iou > tol:
-            bboxes.remove(bbox)
-            tracked.append(obj.advance(bbox))
+    unassigned_objs2 = set()
+
+    for obj in unassigned_objs:
+        if len(bboxes) == 0:
+            unassigned_objs2.add(obj)
         elif len(obj.history) > 0:
             predictor = predict(obj, method)
-            closest_future_bbox = predictor(bbox)
-            future_iou = closest_future_bbox.iou(bbox)
+            bbox, future_iou = predictor.best_match(bboxes)
 
             if future_iou > tol:
+                logger.debug(f"Object {obj.id} assigned detection from trajectory prediction")
                 bboxes.remove(bbox)
-                tracked.append(obj.advance(bbox))
-                predictors.append(predictor)
+                results.append(PredUpdate(obj.advance(bbox), predictor))
+            else:
+                unassigned_objs2.add(obj)
+        else:
+            unassigned_objs2.add(obj)
 
-    # Add the un-matched detections as new objects
+    logger.debug(f"{len(unassigned_objs2)} objects not updated")
+
+    for obj in unassigned_objs:
+        results.append(Unchanged(obj))
+
+    logger.debug(f"{len(bboxes)} detections not assigned to any object")
+
     for bbox in bboxes:
-        tracked.append(Object(bbox))
+        results.append(NewObject(bbox))
 
-    return tracked, predictors
-
-
-def show(
-    frame: cv2.Mat,
-    objects: Iterable[Object],
-    predictors: Iterable[Predictor],
-    *,
-    win_title: str = "Video",
-):
-    for obj in objects:
-        print(obj)
-        obj.draw(frame)
-
-    for predictor in predictors:
-        predictor.draw(frame)
-
-    cv2.imshow(win_title, frame)
+    return results
 
 
-def write(
-    rec: cv2.VideoWriter,
-    frame: cv2.Mat,
-    objects: Iterable[Object],
-    predictors: Iterable[Predictor],
-    *,
-    should_draw: bool = False
-):
-    if should_draw:
-        for obj in objects:
+def draw(frame: cv2.Mat, results: Iterable[TrackingResult]):
+    for result in results:
+        obj = result.obj
+
+        if isinstance(result, (ProxUpdate, PredUpdate)):
             obj.draw(frame)
 
-        for predictor in predictors:
-            predictor.draw(frame)
+        if isinstance(result, PredUpdate):
+            result.predictor.draw(frame, obj.color)
 
-    rec.write(frame)
+        if isinstance(result, Unchanged) and len(obj.history) > 2:
+            gray = (220, 220, 220)
+            line = PLine.from_points(obj.history[0].center, obj.bbox.center)
+            predictor = LinearPredictor(line, obj.bbox)
+
+            obj.bbox.draw(frame, color=gray)
+            predictor.draw(frame, color=gray)
+
+
+def _handle_shutdown(video: cv2.VideoCapture, recording: Optional[cv2.VideoWriter]):
+    def handler(*args: Any):
+        logger = logging.getLogger("exit")
+        logger.debug("SIGINT detected, stopping processing")
+
+        if video.isOpened():
+            video.release()
+
+        if recording and recording.isOpened():
+            recording.release()
+
+        cv2.destroyAllWindows()
+        exit(0)
+
+    return handler
 
 
 @click.command()
-@click.option("--no-show", is_flag=True, help="Do not show live video while running")
+@click.option("--show/--no-show", default=True, help="Do not show live video while running")
 @click.option(
     "--record",
     type=click.Path(exists=False, dir_okay=False, path_type=Path),
@@ -319,19 +424,30 @@ def write(
     show_default=True,
     help="The interpolation method to use",
 )
+@click.option("--debug", is_flag=True, help="Show debugging output during execution")
+@click.option("--labels", multiple=True, help="List of detection labels to accept")
+@click.option("--captures", multiple=True, type=int, help="List of frames to capture into images")
 @click.argument("video", type=click.Path(exists=True, dir_okay=False, path_type=Path))
-def run(video: Path, method: str, no_show: bool, record: Optional[Path]):
+def run(
+    video: Path,
+    method: str,
+    show: bool,
+    record: Optional[Path],
+    debug: bool,
+    labels: list[str],
+    captures: list[int],
+):
+    logger = logging.getLogger("main")
     weights = models.MaskRCNN_ResNet50_FPN_V2_Weights.DEFAULT
     model = models.maskrcnn_resnet50_fpn_v2(weights=weights)
     transforms = weights.transforms()
+    categories = weights.meta["categories"]
     model.eval()
 
+    logging.basicConfig(level=logging.DEBUG if debug else logging.WARN)
+    logger.debug("Starting frame 0 processing")
+
     capture = cv2.VideoCapture(str(video))
-    frames = load_frames(capture)
-    first_frame = next(frames)
-    transformed = transforms(Image.fromarray(first_frame))
-    detections = detect(model, transformed)
-    objects = [Object(box) for box in detections]
 
     if record:
         codec = cv2.VideoWriter_fourcc("M", "J", "P", "G")
@@ -342,22 +458,49 @@ def run(video: Path, method: str, no_show: bool, record: Optional[Path]):
     else:
         recording = None
 
-    for frame in frames:
-        transformed = transforms(Image.fromarray(frame))
-        detections = detect(model, transformed)
-        objects, predictors = track(objects, detections, method=method)
+    signal(SIGINT, _handle_shutdown(capture, recording))
+    signal(SIGTERM, _handle_shutdown(capture, recording))
 
-        if not no_show:
-            show(frame, objects, predictors)
+    frames = load_frames(capture)
+    first_frame = next(frames)
+    transformed = transforms(Image.fromarray(first_frame))
+    detections = detect(model, transformed, labels=categories, allowed=labels)
+    track_results = [NewObject(box) for box in detections]
+
+    draw(first_frame, track_results)
+
+    if show:
+        cv2.imshow("video", first_frame)
+        cv2.waitKey(1)
+
+    if recording:
+        recording.write(first_frame)
+
+    for idx, frame in enumerate(frames, start=1):
+        logger.debug(f"Starting frame {idx} processing")
+
+        transformed = transforms(Image.fromarray(frame))
+        detections = detect(model, transformed, labels=categories, allowed=labels)
+
+        if len(detections) > 0:
+            objects = {result.obj for result in track_results}
+            track_results = track(objects, detections, method=method)
+            draw(frame, track_results)
+
+        if show:
+            cv2.imshow("video", frame)
+            cv2.waitKey(1)
 
         if recording:
-            write(recording, frame, objects, predictors, should_draw=no_show)
+            recording.write(frame)
+
+        if idx in captures:
+            cv2.imwrite(f"{video.stem}_frame{idx}.png", frame)
 
     if recording:
         recording.release()
 
-    if not no_show:
-        cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
